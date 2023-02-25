45d641a3ca8f2d14add1e17864726b774035f7c2
HazaelPoot <hazaelpootmay14705@gmail.com>
2023-02-24 20:06:25 -0600

    Integrated Login View

---
+++ QRDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QRDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
